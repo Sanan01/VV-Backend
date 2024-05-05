42eb92ab9466b708e61b4e9a69f8030d7506818c
Sanan Baig <115987935+Sanan01@users.noreply.github.com>
2024-05-05 17:29:15 +0500

    Update database.js

--- Config/database.js
+++ Config/database.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
-const colors = require('colors');
+const mongoose = require("mongoose");
+const colors = require("colors");
 
-const connectDatabase = async() =>{
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI ,  {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true ,
-            w: "majority"
-        });
+const connectDatabase = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      w: "majority",
+    });
 
-        console.log(`Connected to database : ${conn.connection.host}`.green.bold.underline);
-    }
-    catch(error){
-        console.log(`Error : ${error.message}`.red.bold);
-        process.exit();
-    }
+    console.log(
+      `Connected to database : ${conn.connection.host}`.green.bold.underline
+    );
+  } catch (error) {
+    console.log(`Error : ${error.message}`.red.bold);
+    process.exit();
+  }
 };
 
 module.exports = connectDatabase;
