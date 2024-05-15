ae05fe12987f98d510f7e6ae83643fe87269fb8a
Muhammad Zunique <muhammadzunique4@gmail.com>
2024-05-15 15:28:30 +0500

    IPFS Integration For Votes

--- Controllers/votesControllers.js
+++ Controllers/votesControllers.js
@@ -2,9 +2,95 @@ const asyncHandler = require('express-async-handler');
 const Election = require("../Models/ElectionModel");
 const axios = require('axios');
 
+// const addVoteForElection = asyncHandler(async (req, res) => {
+//     console.log("Add Vote Election Status API");
+//     const { electionId, partyId, candidateId, voterId  } = req.body;
+    
+//     try {
+//         const election = await Election.findOne({ _id: electionId });
+
+//         if (!election) {
+//             return res.status(404).json({ message: 'Election not found' });
+//         }
+
+//         if (!election.isActive) {
+//             return res.status(400).json({ message: 'Cannot add vote, election status not active' });
+//         }
+
+//         const isPartyValid = election.parties.some(party => party.party.toString() === partyId);
+
+//         if (!isPartyValid) {
+//             return res.status(400).json({ message: 'Invalid partyId' });
+//         }
+        
+//         const party = election.parties.find(party => party.party.toString() === partyId);
+
+//         if (!party.candidates.includes(candidateId)) {
+//             return res.status(400).json({ message: 'Invalid candidateId for the specified party' });
+//         }
+
+//         // Check if the voter has already voted in this election
+//         if (election.voters.includes(voterId)) {
+//             return res.status(400).json({ message: 'You have already voted in this election' });
+//         } else {
+//             // Update hasVoted status for the voter in this election
+//             election.voters.push(voterId);
+//         }
+
+//         // Increment vote count
+//         let result = election.results.find(result =>
+//             result.party.toString() === partyId && result.candidate.toString() === candidateId
+//         );
+
+//         if (!result) {
+//             result = {
+//                 party: partyId,
+//                 candidate: candidateId,
+//                 votes: 1
+//             };
+//             election.results.push(result);
+//         } else {
+//             result.votes += 1;
+//         }
+
+//         // Calculate and store hash value
+//         const dataToHash = { electionId, partyId, candidateId, voteCount: result.votes };
+//         const hashObject = {
+//             data: dataToHash,
+//             timestamp: new Date().toISOString()
+//         };
+
+//         const hashResponse = await axios({
+//             method: 'post',
+//             url: 'https://api.pinata.cloud/pinning/pinJSONToIPFS',
+//             data: hashObject,
+//             headers: {
+//                 pinata_api_key: '39672bd0f041ffa01932',
+//                 pinata_secret_api_key: 'f030b1154db8d572bcd5a946438bdc83846a07f637eade929a533e7aaff4488d',
+//                 'Content-Type': 'application/json',
+//             }
+//         });
+
+//         const hashValue = hashResponse.data.IpfsHash;
+//         console.log('Hash Value:', hashValue);
+
+//         // Update hash value and latest vote count in election results
+//         result.latestIPFSHash = hashValue;
+
+//         await election.save();
+
+//         return res.status(200).json({ message: 'Vote added successfully', hashValue });
+
+//     } catch (error) {
+//         console.error(error);
+//         return res.status(500).json({ message: 'Internal server error' });
+//     }
+// });
+
+
 const addVoteForElection = asyncHandler(async (req, res) => {
     console.log("Add Vote Election Status API");
-    const { electionId, partyId, candidateId, voterId } = req.body;
+    const { electionId, partyId, candidateId, voterId , newHash  } = req.body;
     
     try {
         const election = await Election.findOne({ _id: electionId });
@@ -46,46 +132,28 @@ const addVoteForElection = asyncHandler(async (req, res) => {
             result = {
                 party: partyId,
                 candidate: candidateId,
-                votes: 1
+                votes: 1,
+                latestIPFSHash: newHash,
             };
             election.results.push(result);
         } else {
-            result.votes += 1;
+            console.log("Prev Hash >> " , result.latestIPFSHash)
+            result.votes += 1,
+            result.latestIPFSHash= newHash
         }
 
-        // Calculate and store hash value
-        const dataToHash = { electionId, partyId, candidateId, voteCount: result.votes };
-        const hashObject = {
-            data: dataToHash,
-            timestamp: new Date().toISOString()
-        };
-
-        const hashResponse = await axios({
-            method: 'post',
-            url: 'https://api.pinata.cloud/pinning/pinJSONToIPFS',
-            data: hashObject,
-            headers: {
-                pinata_api_key: '39672bd0f041ffa01932',
-                pinata_secret_api_key: 'f030b1154db8d572bcd5a946438bdc83846a07f637eade929a533e7aaff4488d',
-                'Content-Type': 'application/json',
-            }
-        });
-
-        const hashValue = hashResponse.data.IpfsHash;
-        console.log('Hash Value:', hashValue);
-
-        // Update hash value and latest vote count in election results
-        result.latestIPFSHash = hashValue;
-
         await election.save();
+        console.log("New Hash >> " , result.latestIPFSHash)
 
-        return res.status(200).json({ message: 'Vote added successfully', hashValue });
+        return res.status(200).json({ message: 'Vote added successfully' });
 
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Internal server error' });
     }
 });
+
+
 
 
 const getAllVotesByParty = asyncHandler(async (req, res) => {
